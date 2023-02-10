a9ecac5c6db451a27800ddb6bf64bd6fa104d6dd
Connor Albert <connoralbert23@hotmail.com>
2023-02-10 01:06:52 -0600

    Update

--- todoapp/todoapp/urls.py
+++ todoapp/todoapp/urls.py
@@ -5,5 +5,4 @@ from todos.views import index
 urlpatterns = [
     path('', index, name='index'),
     path('todos/', include('todos.urls', namespace="todos")),
-    path('__debug__/', include('debug_toolbar.urls')),
 ]
